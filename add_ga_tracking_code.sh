5645473b39289165e439c593467631bc9b825a27
Doojin Kang <doojin.kang@gmail.com>
2024-10-08 00:45:39 +0900

    cat extension-manager.js

--- add_ga_tracking_code.sh
+++ add_ga_tracking_code.sh
@@ -5,3 +5,12 @@ LF=${LF%_}
 
 mv node_modules/scratch-vm/src/extension-support/extension-manager.js node_modules/scratch-vm/src/extension-support/extension-manager.js_orig
 sed -e "s|this._loadedExtensions.set(extensionURL, serviceName);$|this._loadedExtensions.set(extensionURL, serviceName);${LF}            gtag('event', 'load_extension', {${LF}              'event_category': extensionURL${LF}            });${LF}|g" node_modules/scratch-vm/src/extension-support/extension-manager.js_orig > node_modules/scratch-vm/src/extension-support/extension-manager.js
+
+cat node_modules/scratch-vm/src/extension-support/extension-manager.js
+
+# sed -e "s|this._loadedExtensions.set(extensionURL, serviceName);$|
+#           this._loadedExtensions.set(extensionURL, serviceName);${LF}
+#             gtag('event', 'load_extension', {${LF}
+#             'event_category': extensionURL${LF}
+#             });${LF}
+#         |g" node_modules/scratch-vm/src/extension-support/extension-manager.js_orig > node_modules/scratch-vm/src/extension-support/extension-manager.js
